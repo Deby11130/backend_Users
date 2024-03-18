dff25be18d8bc9ae5d4f2addea123abe8ba50611
Deby11130 <debora.costa.11130@gmail.com>
2024-03-17 21:56:04 -0300

    Comitado

---
+++ backend/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from database import BaseModel
+from sqlalchemy.sql import func
+#cria o a tabela tarefas
+
+
+
+
+#arquivo 2
+
+# #TODO
+class User(BaseModel):
+    __tablename__ = 'users'
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    username = Column('username', String, nullable=False, unique=True)
+    password = Column('password', String, nullable=False)
+    created_at = Column('created_at', DateTime, server_default=func.now())
+    updated_at = Column('updated_at', DateTime, onupdate=func.now())
+
+
+#arquivo 2
---
+++ backend/schema.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class UserSchema(BaseModel):
+    id: int
+    username: str
+    password: str 
+
+#arquivo 3
